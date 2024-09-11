98d4f0f0c7b3eed1df17bd55e11b1e036cf431db
Eesaa Mahmood <tr015314@student.reading.ac.uk>
2024-09-11 17:23:31 +0100

    modified main so that it returns the ratio properly and added prices dictionary so it can be parsed into getRatio

--- client3.py
+++ client3.py
@@ -43,7 +43,7 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if (price_b == 0):
-        raise ValueError("Cannot divide by 0")
+        return
     #can't divide by 0 so raise error
     return price_a/price_b
 
@@ -53,10 +53,14 @@ if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
+        prices= {}
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
+          
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+
